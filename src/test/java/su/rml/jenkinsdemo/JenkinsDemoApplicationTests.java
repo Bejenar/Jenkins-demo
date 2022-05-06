e7ac05981d03469c4722fa3fbc5b1030bedc7045
Bejenar Nicolai <admynyral2808@gmail.com>
2022-05-06 20:02:45 +0300

    first commit

---
+++ src/test/java/su/rml/jenkinsdemo/JenkinsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package su.rml.jenkinsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
